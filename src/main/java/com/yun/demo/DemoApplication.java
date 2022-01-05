05c1a0286400865da4e735b469ec89d8cd60c378
tanshiyun <2031347096@qq.com>
2022-01-05 23:15:12 +0800

    datasource and log

--- src/main/java/com/yun/demo/DemoApplication.java
+++ src/main/java/com/yun/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.yun.demo;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
