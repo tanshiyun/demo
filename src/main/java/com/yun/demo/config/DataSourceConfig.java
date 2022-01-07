2909dfe24c45ea95a5a5710aba0c5b8480d161a3
tanshiyun <tanshiyun@tongyu.tech>
2022-01-07 17:03:30 +0800

    alibaba druid

---
+++ src/main/java/com/yun/demo/config/DataSourceConfig.java
@@ -0,0 +1,38 @@
+package com.yun.demo.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.datasource")
+@Setter
+public class DataSourceConfig {
+
+    private String url;
+    private String username;
+    private String password;
+    private String driverClassName;
+
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    public DruidDataSource datasource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(url);
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(DruidDataSource dataSource){
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+}
