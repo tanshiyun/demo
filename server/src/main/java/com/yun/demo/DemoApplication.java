fa774d34e835520621237a5cbccf2c5db7c07bcc
tanshiyun <2031347096@qq.com>
2022-01-08 20:59:59 +0800

    use druid auto configuration

--- server/src/main/java/com/yun/demo/DemoApplication.java
+++ server/src/main/java/com/yun/demo/DemoApplication.java
@@ -4,9 +4,11 @@ import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
+@SpringBootApplication
+@ComponentScan("com.yun")
 public class DemoApplication {
 
 	public static void main(String[] args) {
